5898d68e3f90aeb34846602cbe6102b8e312b760
= <=>
2020-05-15 00:59:18 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola usuario por favor digite seu nome!");
+
+            string nome = Console.ReadLine();
+            string mensagem = "Olá " + nome + "!";
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
